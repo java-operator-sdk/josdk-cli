f75e765b33ea07ad426f4215b03c52ff1996b989
soroosh <soroosh.sarabadani@gmail.com>
2020-12-17 12:22:16 +0100

    add parent command (Jops)

---
+++ src/main/java/Jops.java
@@ -0,0 +1,11 @@
+import picocli.CommandLine;
+
+@CommandLine.Command(subcommands = {InitCommand.class})
+public class Jops implements Runnable {
+    public void run() {
+    }
+
+    public static void main(String[] args) {
+        System.exit(new CommandLine(new Jops()).execute(args));
+    }
+}
