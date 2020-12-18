01de2775e9e28a974a55da8b90414be1ec8066d9
soroosh <soroosh.sarabadani@gmail.com>
2020-12-18 09:55:48 +0100

    set the final name for the jar

--- src/main/java/io/javaoperatorsdk/cli/Jops.java
+++ src/main/java/io/javaoperatorsdk/cli/Jops.java
@@ -3,10 +3,8 @@ package io.javaoperatorsdk.cli;
 import io.javaoperatorsdk.cli.commands.InitCommand;
 import picocli.CommandLine;
 
-@CommandLine.Command(subcommands = {InitCommand.class})
-public class Jops implements Runnable {
-    public void run() {
-    }
+@CommandLine.Command(subcommands = {InitCommand.class}, customSynopsis = "Usage: jops [COMMAND]")
+public class Jops {
 
     public static void main(String[] args) {
         System.exit(new CommandLine(new Jops()).execute(args));
