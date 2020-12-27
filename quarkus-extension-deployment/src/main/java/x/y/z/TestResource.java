94f91df5129c2e7a3fd3c0cfb059393bcc434339
soroosh <soroosh.sarabadani@gmail.com>
2020-12-27 23:45:03 +0100

    use matcher in TestResource

--- quarkus-extension-deployment/src/main/java/x/y/z/TestResource.java
+++ quarkus-extension-deployment/src/main/java/x/y/z/TestResource.java
@@ -1,8 +1,21 @@
 package x.y.z;
 
 import io.quarkus.runtime.annotations.RecordableConstructor;
+import java.net.URL;
+import java.util.List;
 
 public class TestResource {
+  private List<URL> urls;
+
   @RecordableConstructor
   public TestResource() {}
+
+  @RecordableConstructor
+  public TestResource(List<URL> urls) {
+    this.urls = urls;
+  }
+
+  public List<URL> getUrls() {
+    return urls;
+  }
 }
--- quarkus-extension-deployment/src/main/java/x/y/z/TestResourceRecorder.java
+++ quarkus-extension-deployment/src/main/java/x/y/z/TestResourceRecorder.java
@@ -1,15 +1,45 @@
 package x.y.z;
 
 import io.quarkus.runtime.annotations.Recorder;
+import java.io.IOException;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 import java.util.function.Supplier;
+import java.util.stream.Collectors;
+import org.springframework.core.io.Resource;
 
 @Recorder
 public class TestResourceRecorder {
+  private List<URL> resourcePaths = new ArrayList<>();
+
+  public TestResourceRecorder() {
+    X s = new X();
+    try {
+      Resource[] resources = s.getResources("templates/**");
+      resourcePaths =
+          Arrays.stream(resources)
+              .map(
+                  r -> {
+                    try {
+                      return r.getURL();
+                    } catch (IOException e) {
+                      e.printStackTrace();
+                      return null;
+                    }
+                  })
+              .collect(Collectors.toList());
+    } catch (IOException e) {
+      e.printStackTrace();
+    }
+  }
+
   public Supplier<TestResource> getSupplier() {
     return new Supplier<TestResource>() {
       @Override
       public TestResource get() {
-        return new TestResource();
+        return new TestResource(resourcePaths);
       }
     };
   }
