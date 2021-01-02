b3e85ed3f0bd1dfb59aa00ceccb2fcb82922fbdd
soroosh <soroosh.sarabadani@gmail.com>
2021-01-03 00:18:52 +0100

    use reflections8 instead of spring

--- quarkus-extension-deployment/src/main/java/io/javaoperatorsdk/quarkus/resources/TestResourceRecorder.java
+++ quarkus-extension-deployment/src/main/java/io/javaoperatorsdk/quarkus/resources/TestResourceRecorder.java
@@ -1,14 +1,15 @@
 package io.javaoperatorsdk.quarkus.resources;
 
 import io.quarkus.runtime.annotations.Recorder;
+
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 import java.util.function.Supplier;
 import java.util.stream.Collectors;
-import org.springframework.core.io.Resource;
-import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+
+import org.reflections8.Reflections;
+import org.reflections8.scanners.ResourcesScanner;
+import org.reflections8.util.Utils;
 
 @Recorder
 public class TestResourceRecorder {
@@ -16,25 +17,48 @@ public class TestResourceRecorder {
   private List<String> resourcePaths = new ArrayList<>();
 
   public TestResourceRecorder() {
-    PathMatchingResourcePatternResolver s = new PathMatchingResourcePatternResolver();
+    //        PathMatchingResourcePatternResolver s = new PathMatchingResourcePatternResolver();
+    //        try {
+    //            Resource[] resources = s.getResources("templates/**");
+    //            resourcePaths =
+    //                    Arrays.stream(resources)
+    //                            .map(
+    //                                    r -> {
+    //                                        try {
+    //                                            final var splitted =
+    // r.getURL().getPath().split("!");
+    //                                            return splitted[splitted.length - 1];
+    //                                        } catch (IOException e) {
+    //                                            e.printStackTrace();
+    //                                            return null;
+    //                                        }
+    //                                    })
+    //                            .collect(Collectors.toList());
+    //        } catch (IOException e) {
+    //            e.printStackTrace();
+    //        }
+
     try {
-      Resource[] resources = s.getResources("templates/**");
-      resourcePaths =
-          Arrays.stream(resources)
-              .map(
-                  r -> {
-                    try {
-                      final var splitted = r.getURL().getPath().split("!");
-                      return splitted[splitted.length - 1];
-                    } catch (IOException e) {
-                      e.printStackTrace();
-                      return null;
-                    }
-                  })
-              .collect(Collectors.toList());
+      List<String> resources = getResourceFiles("templates");
+      resourcePaths.addAll(resources);
     } catch (IOException e) {
       e.printStackTrace();
     }
+  }
+
+  private List<String> getResourceFiles(String path) {
+    Reflections reflections = new Reflections(null, new ResourcesScanner());
+    final var resources =
+        reflections.getStore().get(Utils.index(ResourcesScanner.class)).values().stream()
+            .flatMap(Set::stream)
+            .filter(
+                p -> {
+                  System.out.println(p);
+                  return p.contains(path);
+                })
+            .collect(Collectors.toList());
+
+    return new ArrayList<>(resources);
   }
 
   public Supplier<ResourceLocator> getSupplier() {
